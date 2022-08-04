5162d1e872956d345196b72d3243192056756d94
Muriithi Ian <imuriithiian@gmail.com>
2022-08-04 10:41:58 +0300

    countdown with while loop

--- index.js
+++ index.js
@@ -9,10 +9,30 @@ const gifts = ["teddy bear", "drone", "doll"];
 function wrapGifts(gifts) {
 	for (let i = 0; i < gifts.length; i++) {
 		console.log(`Wrapped ${gifts[i]} and added a bow!`);
-		debugger;
+		// debugger;
 	}
 
 	return gifts;
 }
 
 wrapGifts(gifts);
+
+function writeCards(namesArr, ocassion) {
+	const cards = [];
+	for (let i = 0; i < namesArr.length; i++) {
+		let message = `Thank you, ${namesArr[i]}, for the wonderful ${ocassion} gift!`;
+		cards.push(message);
+	}
+	return cards;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(count) {
+	while (count >= 0) {
+		console.log(count);
+		count--;
+	}
+}
+
+countDown(10);
